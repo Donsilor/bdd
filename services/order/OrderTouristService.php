6c994448178224c024526fd032b941e653956d1c
zhengzhufu <fuzhuzheng@163.com>
2020-04-01 15:52:39 +0800

    统一费用入口

--- services/order/OrderTouristService.php
+++ services/order/OrderTouristService.php
@@ -34,44 +34,15 @@ class OrderTouristService extends OrderBaseService
      */
     public function createOrder($cartList, $invoice_info)
     {
-        //IP区域ID与地址
-        list($ip_area_id, $ip_location) = \Yii::$app->ipLocation->getLocation(\Yii::$app->request->userIP);
+        $orderAccountTax = $this->getCartAccountTax($cartList);
 
-        $goods_amount = 0;
-        $details = [];
-        foreach ($cartList as $item) {
-            $goods = \Yii::$app->services->goods->getGoodsInfo($item['goods_id'], $item['goods_type']);
-
-            //商品价格
-            $sale_price = $this->exchangeAmount($goods['sale_price']*$item['goods_num']);
-            $goods_amount += $sale_price;
-
-            $detail = new OrderTouristDetails();
-            $detail->style_id = $goods['style_id'];//商品ID
-            $detail->goods_id = $item['goods_id'];//商品ID
-            $detail->goods_sn = $goods['goods_sn'];//商品编号
-            $detail->goods_type = $goods['type_id'];//产品线
-            $detail->goods_name = $goods['goods_name'];//价格
-            $detail->goods_price = $sale_price;//价格
-            $detail->goods_num = $item['goods_num'];//数量
-            $detail->goods_image = $goods['goods_image'];//商品图片
-            $detail->promotions_id = 0;//$item['promotions_id'];//促销活动ID
-            $detail->group_id = $item['group_id'];//组ID
-            $detail->group_type = $item['group_type'];//分组类型
-            $detail->goods_spec = $goods['goods_spec'];//商品规格
-            $detail->goods_attr = $goods['goods_attr'];//商品规格
-
-            $details[] = $detail;
+        //商品列表
+        if(empty($orderAccountTax['orderGoodsList'])) {
+            throw new UnprocessableEntityHttpException("商品信息为空");
         }
 
-        //金额
-        $discount_amount = 0;//优惠金额
-        $shipping_fee = 0;//运费
-        $tax_fee = 0;//税费
-        $safe_fee = 0;//保险费
-        $other_fee = 0;//其他费用
-
-        $order_amount = $goods_amount + $shipping_fee + $tax_fee + $safe_fee + $other_fee;//订单总金额
+        //IP区域ID与地址
+        list($ip_area_id, $ip_location) = \Yii::$app->ipLocation->getLocation(\Yii::$app->request->userIP);
 
         //保存订单信息
         $order = new OrderTourist();
@@ -80,18 +51,18 @@ class OrderTouristService extends OrderBaseService
         $order->store_id = null;//店铺ID
         $order->tourist_key = null;//游客的KEY
 
-        $order->order_amount = $order_amount;//订单金额
-        $order->goods_amount = $goods_amount;//商品总金额
-        $order->discount_amount = $discount_amount;//优惠金额
+        $order->order_amount = $orderAccountTax['order_amount'];//订单金额
+        $order->goods_amount = $orderAccountTax['goods_amount'];//商品总金额
+        $order->discount_amount = $orderAccountTax['discount_amount'];//优惠金额
         $order->pay_amount = 0;//实际支付金额
         $order->refund_amount = 0;//退款金额
-        $order->shipping_fee = $shipping_fee;//运费
-        $order->tax_fee = $tax_fee;//税费
-        $order->safe_fee = $safe_fee;//保险费
-        $order->other_fee = $other_fee;//附加费
+        $order->shipping_fee = $orderAccountTax['shipping_fee'];//运费
+        $order->tax_fee = $orderAccountTax['tax_fee'];//税费
+        $order->safe_fee = $orderAccountTax['safe_fee'];//保险费
+        $order->other_fee = $orderAccountTax['other_fee'];//附加费
 
-        $order->currency = $this->getCurrency();//货币
-        $order->exchange_rate = $this->getExchangeRate();//汇率
+        $order->currency = $orderAccountTax['currency'];//货币
+        $order->exchange_rate = $orderAccountTax['exchange_rate'];//汇率
         $order->language   = $this->getLanguage();//语言
         $order->ip = \Yii::$app->request->userIP;  //用户下单ip
 
@@ -105,7 +76,11 @@ class OrderTouristService extends OrderBaseService
             throw new UnprocessableEntityHttpException($this->getError($order));
         }
 
-        foreach ($details as $detail) {
+        $orderGoodsList = $orderAccountTax['orderGoodsList'];
+        foreach ($orderGoodsList as $goods) {
+            $detail = new OrderTouristDetails();
+            $detail->attributes = $goods;
+
             //订单ID
             $detail->order_tourist_id = $order->id;
 
