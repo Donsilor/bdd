6e780b50902e26d6ded3202a624235687a48c45a
祝.福 <fuzhuzheng@163.com>
2020-04-26 15:46:22 +0800

    log

--- backend/modules/order/views/order-log/index.php
+++ backend/modules/order/views/order-log/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 use common\enums\OrderStatusEnum;
 use kartik\daterange\DateRangePicker;
 
-$this->title = Yii::t('order', '游客订单');
+$this->title = Yii::t('order', '客户订单');
 $this->params['breadcrumbs'][] = ['label' => $this->title];
 
 ?>
@@ -14,6 +14,14 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
 <div class="row">
     <div class="col-sm-12">
         <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+                <li>
+                    <a href="<?= Url::to(['order/view', 'id'=>$model->id]) ?>"> <?= Html::encode($this->title) ?></a>
+                </li>
+                <li class="active">
+                    <a href="<?= Url::to(['order-log/index', 'id'=>$model->id]) ?>"> <?= Html::encode('日志记录') ?></a>
+                </li>
+            </ul>
             <div class="tab-content">
                 <div class="active tab-pane">
                     <?= GridView::widget([
@@ -32,67 +40,7 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
                                 'attribute' => 'id',
                             ],
                             [
-                                'attribute' => 'created_at',
-                                'filter' => DateRangePicker::widget([    // 日期组件
-                                    'model' => $searchModel,
-                                    'attribute' => 'created_at',
-                                    'value' => '',
-                                    'options' => ['readonly' => true, 'class' => 'form-control',],
-                                    'pluginOptions' => [
-                                        'format' => 'yyyy-mm-dd',
-                                        'locale' => [
-                                            'separator' => '/',
-                                        ],
-                                        'endDate' => date('Y-m-d', time()),
-                                        'todayHighlight' => true,
-                                        'autoclose' => true,
-                                        'todayBtn' => 'linked',
-                                        'clearBtn' => true,
-                                    ],
-                                ]),
-                                'value' => function ($model) {
-                                    return Yii::$app->formatter->asDatetime($model->created_at);
-                                },
-                                'format' => 'raw',
-                            ],
-                            [
-                                'attribute' => 'order_sn',
-                            ],
-                            [
-                                'attribute' => 'order_amount',
-                                'value' => function ($model) {
-                                    return sprintf('(%s)%s', $model->currency, $model->order_amount);
-                                }
-                            ],
-                            [
-                                'attribute' => 'ip',
-                                'value' => function ($model) {
-                                    return $model->ip."(".$model->ip_location.")";
-                                },
-                            ],
-                            [
-                                'attribute' => 'ip_area_id',
-                                'headerOptions' => ['class' => 'col-md-1'],
-                                'filter' => Html::activeDropDownList($searchModel, 'ip_area_id', \common\enums\AreaEnum::getMap(), [
-                                    'prompt' => '全部',
-                                    'class' => 'form-control',
-                                ]),
-                                'value' => function ($model) {
-                                    return \common\enums\AreaEnum::getValue($model->ip_area_id);
-                                },
-                                'format' => 'raw',
-                            ],
-                            [
-                                'attribute' => 'status',
-                                'headerOptions' => ['class' => 'col-md-1'],
-                                'filter' => Html::activeDropDownList($searchModel, 'status', ['未支付', '已支付'], [
-                                    'prompt' => '全部',
-                                    'class' => 'form-control',
-                                ]),
-                                'value' => function ($model) {
-                                    return array_get(['未支付', '已支付'], $model->status);
-                                },
-                                'format' => 'raw',
+                                'log_time',
                             ],
                         ],
                     ]);
