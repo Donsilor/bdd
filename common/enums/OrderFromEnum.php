11088c081b86efdbaa7f119f7bf873b3efcc1d56
gaopeng <763429951@qq.com>
2020-05-06 16:50:25 +0800

    fix

--- common/enums/OrderFromEnum.php
+++ common/enums/OrderFromEnum.php
@@ -9,17 +9,24 @@ namespace common\enums;
  */
 class OrderFromEnum extends BaseEnum
 {
-    const FROM_WEB = 1;
-    const FROM_MOBILE = 2;
-    
+    const WEB_HK = 10;
+    const MOBILE_HK = 11;
+    const WEB_CN = 20;
+    const MOBILE_CN = 21;
+    const WEB_US = 30;
+    const MOBILE_US = 31;
     /**
      * @return array
      */
     public static function getMap(): array
     {
         return [
-                self::FROM_WEB => '官网PC端',
-                self::FROM_MOBILE => '官网手机端',                
+                self::WEB_HK => '香港站PC端',
+                self::MOBILE_HK => '香港站移动端', 
+                self::WEB_CN => '大陆站PC端',
+                self::MOBILE_CN => '大陆站移动端', 
+                self::WEB_US => '美国站PC端',
+                self::MOBILE_US => '美国站移动端', 
         ];
     }
 }
