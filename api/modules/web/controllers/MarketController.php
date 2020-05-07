e26b454ed73b5ea3c198723db3a1d00aa3c3618a
祝.福 <fuzhuzheng@163.com>
2020-05-07 10:06:20 +0800

    页

---
+++ api/modules/web/controllers/MarketController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace api\modules\web\controllers;
+
+
+use api\controllers\ActiveController;
+
+class MarketController extends ActiveController
+{
+
+    
+
+    public function actionIndex()
+    {
+
+    }
+}
