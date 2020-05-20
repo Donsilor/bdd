8a9fc6d2ac603a1235075dbd7d224de2168b0d33
祝.福 <fuzhuzheng@163.com>
2020-05-20 10:02:30 +0800

    NotifyContacts

--- services/common/NotifyContactsService.php
+++ services/common/NotifyContactsService.php
@@ -5,9 +5,29 @@ namespace services\common;
 
 
 use common\components\Service;
+use common\models\common\NotifyContacts;
 
 class NotifyContactsService extends Service
 {
+    //获取被通知人信息
+    static public function getNotifyContactsInfo($typeId)
+    {
+        $data = NotifyContacts::find()->where(['type_id'=>$typeId])->all();
+
+        $mobiles = [];
+        $emails = [];
+        foreach ($data as $datum) {
+            if($datum['email_switch']) {
+                $emails[] = $datum['email'];
+            }
+            if($datum['mobile_switch']) {
+                $mobiles[] = $datum['mobile'];
+            }
+        }
+
+        return ['mobiles'=>$mobiles, 'emails'=>$emails];
+    }
+
     //订单付款成功时执行
     static public function orderPaySuccess($orderSn)
     {
