f68cd3dea826943c46d2b461441679d2046e9073
祝.福 <fuzhuzheng@163.com>
2020-06-08 11:17:05 +0800

    dev_253

---
+++ backend/modules/statistics/controllers/StyleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\modules\statistics\controllers;
+
+
+use backend\controllers\BaseController;
+
+class StyleController extends BaseController
+{
+    public function actionIndex() {
+
+    }
+}
