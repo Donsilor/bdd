717e4dba3935b161b85ab4a5ee18d656f0ba1543
flan <fuzhuzheng@163.com>
2020-09-02 16:00:06 +0800

    dev_141

--- backend/modules/order/controllers/OrderCommentController.php
+++ backend/modules/order/controllers/OrderCommentController.php
@@ -3,16 +3,56 @@
 
 namespace backend\modules\order\controllers;
 
-
+use Yii;
 use backend\controllers\BaseController;
+use common\enums\OrderFromEnum;
+use common\models\base\SearchModel;
+use common\models\order\OrderComment;
+use common\models\order\OrderTourist;
 
 class OrderCommentController extends BaseController
 {
+    /**
+     * @var OrderTourist
+     */
+    public $modelClass = OrderComment::class;
+
+
     public function actionIndex()
     {
+        $searchModel = new SearchModel([
+            'model' => $this->modelClass,
+            'scenario' => 'default',
+            'partialMatchAttributes' => [], // 模糊查询
+            'defaultOrder' => [
+                'id' => SORT_DESC,
+            ],
+            'pageSize' => $this->pageSize,
+            'relations' => []
+        ]);
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams, ['created_at']);
+
+        //创建时间过滤
+        if (!empty(Yii::$app->request->queryParams['SearchModel']['created_at'])) {
+            list($start_date, $end_date) = explode('/', Yii::$app->request->queryParams['SearchModel']['created_at']);
+            $dataProvider->query->andFilterWhere(['between', 'created_at', strtotime($start_date), strtotime($end_date) + 86400]);
+        }
+
+        //站点地区
+//        $sitesAttach = \Yii::$app->getUser()->identity->sites_attach;
+//        if(is_array($sitesAttach)) {
+//            $orderFroms = [];
+//
+//            foreach ($sitesAttach as $site) {
+//                $orderFroms = array_merge($orderFroms, OrderFromEnum::platformsForGroup($site));
+//            }
+//
+//            $dataProvider->query->andWhere(['in', 'order_tourist.order_from', $orderFroms]);
+//        }
+
         return $this->render($this->action->id, [
-//            'dataProvider' => $dataProvider,
-//            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
         ]);
     }
 }
