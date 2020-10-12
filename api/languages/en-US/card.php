39b6db59abc104abc6292926da6eecbfb5dceeb9
祝.福 <fuzhuzheng@163.com>
2020-10-12 09:34:06 +0800

    dev_351

--- api/languages/en-US/card.php
+++ api/languages/en-US/card.php
@@ -4,6 +4,6 @@ return [
     '香港' => 'Hong Kong',
     '澳门' => 'Macao',
     '台湾' => 'Taiwan',
-    '国外' => 'Foreign Site  Use Only',
+    '国外' => 'Foreign',
     '仅限[%s]站点使用' => '[%s]Site  Use Only',
 ];
