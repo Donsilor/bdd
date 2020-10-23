6c24a274917a2e7a22b604b9bd6226004801a397
flan <fuzhuzheng@163.com>
2020-10-23 12:14:54 +0800

    dev_418

--- backend/modules/statistics/controllers/OrderController.php
+++ backend/modules/statistics/controllers/OrderController.php
@@ -5,15 +5,109 @@ namespace backend\modules\statistics\controllers;
 
 
 use backend\controllers\BaseController;
+use common\models\base\SearchModel;
+use common\models\statistics\OrderView;
+use Yii;
 
 class OrderController extends BaseController
 {
 
+
+    /**
+     * @var OrderView
+     */
+    public $modelClass = OrderView::class;
+
     public function actionIndex()
     {
+//        $time = time();
+
+//        $start_time = 0;
+//        $end_time = $time;
+
+//        $order = <<<DOM
+//(SELECT `og`.`style_id`,COUNT(`og`.`style_id`) AS count,(CASE `o`.`order_from` WHEN 10 THEN 'HK'
+//                        WHEN 11 THEN 'HK'
+//                        WHEN 20 THEN 'CN'
+//                        WHEN 21 THEN 'CN'
+//                        WHEN 30 THEN 'US'
+//                        WHEN 31 THEN 'US'
+//              END) AS order_from
+//FROM `order` `o`
+//RIGHT JOIN `order_goods` AS `og` ON  `o`.`id`=`og`.`order_id`
+//WHERE `o`.`created_at` BETWEEN :start_time and :end_time and order_status>10 GROUP BY `og`.`style_id`,CASE `o`.`order_from` WHEN 10 THEN 'HK' WHEN 11 THEN 'HK' WHEN 20 THEN 'CN' WHEN 21 THEN 'CN' WHEN 30 THEN 'US' WHEN 31 THEN 'US' END) AS og
+//DOM;
+
+//        $orderCart = <<<DOM
+//(SELECT COUNT(`oc`.`style_id`) as count,oc.style_id,oc.platform_group FROM `order_cart` oc WHERE `created_at` BETWEEN :start_time and :end_time GROUP BY `oc`.`style_id`, `oc`.`platform_group`) as oc
+//DOM;
+
+        $searchModel = new SearchModel([
+            'model' => $this->modelClass,
+            'scenario' => 'default',
+            'partialMatchAttributes' => [], // 模糊查询
+            'pageSize' => $this->pageSize,
+            'relations' => []
+        ]);
+
+        $queryParams = Yii::$app->request->queryParams;
+
+        //站点地区
+//        if(isset($queryParams['SearchModel']['platform_group']) && !empty($queryParams['SearchModel']['platform_group'])) {
+//            $queryParams['SearchModel']['platform_group'] = implode(',', $queryParams['SearchModel']['platform_group']);
+//        }
+
+        //时间
+//        if(isset($queryParams['SearchModel']['datetime']) && !empty($queryParams['SearchModel']['datetime'])) {
+//            list($start_time, $end_time) = explode('/', $queryParams['SearchModel']['datetime']);
+//            $start_time = strtotime($start_time);
+//            $end_time = strtotime($end_time)+86400;
+//        }
+
+        $dataProvider = $searchModel->search($queryParams, ['datetime']);
+
+//        $dataProvider->query->leftJoin($order, 'statistics_style_view.style_id=og.style_id and og.order_from=statistics_style_view.platform_group', ['start_time'=>$start_time, 'end_time'=>$end_time]);
+//        $dataProvider->query->leftJoin($orderCart, 'statistics_style_view.style_id=oc.style_id and oc.platform_group=statistics_style_view.platform_group', ['start_time'=>$start_time, 'end_time'=>$end_time]);
+
+        $dataProvider->query->select(['statistics_order_view.*']);
+
+//        $dataProvider->query->andWhere(['or', ['>','og.count','0'], ['>','oc.count','0']]);
+
+        $dataProvider->query->asArray();
+
+//        $dataProvider->setSort([
+//            'attributes' => [
+//                'count',
+//                'cart_count',
+//                'style_id',
+//                'type_id',
+//                'style_name',
+//                'platform_group',
+//                'name',
+//            ],
+//            'defaultOrder' => [
+//                'style_id' => 'desc',
+//                'platform_group' => 'desc',
+//            ],
+//        ]);
+
+        //导出
+//        if(Yii::$app->request->get('action') === 'export'){
+//            $query = Yii::$app->request->queryParams;
+//            unset($query['action']);
+//            if(empty(array_filter($query))){
+//                return $this->message('导出条件不能为空', $this->redirect(['index']), 'warning');
+//            }
+//            $dataProvider->setPagination(false);
+//            $list = $dataProvider->models;
+//            $this->getExport($list);
+//        }
+
+//        $searchModel->platform_group = Yii::$app->request->queryParams['SearchModel']['platform_group']??[];
+
         return $this->render($this->action->id, [
-//            'dataProvider' => $dataProvider,
-//            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
         ]);
     }
 }
---
+++ common/models/statistics/OrderView.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace common\models\statistics;
+
+use common\models\order\OrderAccount;
+use Yii;
+
+/**
+ * This is the model class for table "{{%statistics_style_view}}".
+ *
+ * @property int $style_id 款式ID
+ * @property int $type_id 产品线
+ * @property string $style_name 款式名称
+ * @property string $platform
+ * @property string $platform_group
+ * @property string $name
+ */
+class OrderView extends \common\models\base\BaseModel
+{
+
+    public $datetime;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%statistics_order_view}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['platform_id', 'status'], 'integer'],
+            [['datetime'], 'string', 'max' => 300],
+            [['platform_group'], 'string', 'max' => 2],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => '款式ID',
+            'platform_group' => '站点地区',
+            'platform_id' => '客户端',
+            'status' => '状态'
+        ];
+    }
+
+//    public static function primaryKey()
+//    {
+//        return ['style_id','platform_group'];
+//    }
+
+//    public function getOg()
+//    {
+//        $order = <<<DOM
+//(SELECT `og`.`style_id`,COUNT(`og`.`style_id`) AS count,`o`.`order_from`
+//FROM `order` `o`
+//RIGHT JOIN `order_goods` AS `og` ON  `o`.`id`=`og`.`order_id`
+//WHERE 1 GROUP BY `og`.`style_id`,`o`.`order_from`) AS og
+//DOM;
+//        return $this->hasOne($order, ['order_id'=>'id']);
+//    }
+}
