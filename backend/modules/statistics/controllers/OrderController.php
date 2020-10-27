6fc92ebe3626334ec3064374331f4a2d4e33d11c
祝.福 <fuzhuzheng@163.com>
2020-10-27 11:44:58 +0800

    dev_418

--- backend/modules/statistics/controllers/OrderController.php
+++ backend/modules/statistics/controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace backend\modules\statistics\controllers;
 
 
 use backend\controllers\BaseController;
+use common\helpers\ExcelHelper;
 use common\models\base\SearchModel;
 use common\models\statistics\OrderView;
 use Yii;
@@ -24,23 +25,6 @@ class OrderController extends BaseController
 
         $start_time = strtotime(date('Y-m-01'));
         $end_time = $time;
-
-//        $order = <<<DOM
-//(SELECT `og`.`style_id`,COUNT(`og`.`style_id`) AS count,(CASE `o`.`order_from` WHEN 10 THEN 'HK'
-//                        WHEN 11 THEN 'HK'
-//                        WHEN 20 THEN 'CN'
-//                        WHEN 21 THEN 'CN'
-//                        WHEN 30 THEN 'US'
-//                        WHEN 31 THEN 'US'
-//              END) AS order_from
-//FROM `order` `o`
-//RIGHT JOIN `order_goods` AS `og` ON  `o`.`id`=`og`.`order_id`
-//WHERE `o`.`created_at` BETWEEN :start_time and :end_time and order_status>10 GROUP BY `og`.`style_id`,CASE `o`.`order_from` WHEN 10 THEN 'HK' WHEN 11 THEN 'HK' WHEN 20 THEN 'CN' WHEN 21 THEN 'CN' WHEN 30 THEN 'US' WHEN 31 THEN 'US' END) AS og
-//DOM;
-
-//        $orderCart = <<<DOM
-//(SELECT COUNT(`oc`.`style_id`) as count,oc.style_id,oc.platform_group FROM `order_cart` oc WHERE `created_at` BETWEEN :start_time and :end_time GROUP BY `oc`.`style_id`, `oc`.`platform_group`) as oc
-//DOM;
 
         $searchModel = new SearchModel([
             'model' => $this->modelClass,
@@ -83,27 +67,186 @@ class OrderController extends BaseController
 
 //        $dataProvider->query->select(['statistics_order_view.*']);
 
-//        $dataProvider->query->asArray();
-
-        //导出
-//        if(Yii::$app->request->get('action') === 'export'){
-//            $query = Yii::$app->request->queryParams;
-//            unset($query['action']);
-//            if(empty(array_filter($query))){
-//                return $this->message('导出条件不能为空', $this->redirect(['index']), 'warning');
-//            }
-//            $dataProvider->setPagination(false);
-//            $list = $dataProvider->models;
-//            $this->getExport($list);
-//        }
-
         $searchModel->platform_group = Yii::$app->request->queryParams['SearchModel']['platform_group']??[];
         $searchModel->platform_id = Yii::$app->request->queryParams['SearchModel']['platform_id']??'';
         $searchModel->datetime = date('Y-m-d', $start_time) . '/' . date('Y-m-d', $end_time);
+
+        //导出
+        if (Yii::$app->request->get('action') === 'export') {
+//            $query = Yii::$app->request->queryParams;
+//            unset($query['action']);
+//            if (empty(array_filter($query))) {
+//                return $this->message('导出条件不能为空', $this->redirect(['index']), 'warning');
+//            }
+            $dataProvider->setPagination(false);
+            $list = $dataProvider->models;
+            $this->getExport($list, $searchModel);
+        }
 
         return $this->render($this->action->id, [
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel,
         ]);
+    }
+
+    public function getExport($list, $searchModel)
+    {
+        $status = [
+            '1' => '未付款',
+            '2' => '已销售',
+            '3' => '已关闭',
+        ];
+
+        $typeMap = [];
+        foreach ($list as $item) {
+            //时间段
+            //站点地区
+            //客户端
+            //订单状态
+            //订单总数量
+            //订单总额
+            //{}
+            //商品总量
+            //{}
+            $data = $item->getOrderProductTypeGroupData($searchModel);
+
+            $typeMap[$item->id] = [];
+            foreach ($data as $datum) {
+                $typeMap[$item->id][$datum['goods_type']] = $datum;
+            }
+        }
+
+        // [名称, 字段名, 类型, 类型规则]
+        $header = [
+            ['时间段', 'id', 'function', function($row) use($searchModel) {
+                return $searchModel->datetime;
+            }],
+            ['站点地区', 'platform_group', 'function', function($row) {
+                return \common\enums\OrderFromEnum::getValue($row['platform_group'], 'groups');
+            }],
+            ['客户端', 'platform_id', 'function', function($row) {
+                return \common\enums\OrderFromEnum::getValue($row['platform_id']);
+            }],
+            ['订单状态', 'platform_group', 'function', function($row) use ($status) {
+                return $status[$row['status']]??'';
+            }],
+            ['订单总数量', 'id', 'function', function($row) use($searchModel) {
+                return $row->getOrderCount($searchModel);
+            }],
+
+            ['订单总额（CNY）', 'id', 'function', function($row) use($searchModel) {
+                return $row->getOrderMoneySum($searchModel);
+            }],
+            ['戒指', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][2]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['对戒', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][19]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['裸钻', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][15]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['戒托', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][12]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['项链', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][4]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['吊坠', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][5]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['手链', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][8]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['手镯', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][9]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['耳钉', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][6]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['耳环', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][7]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['挂件', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][17]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['摆件', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][16]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+            ['其他', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][18]??[];
+                return sprintf("%.2f", $typeData['sum']??0);
+            }],
+
+            ['商品总量', 'id', 'function', function($row) use($searchModel) {
+                return $row->getOrderProductCount($searchModel);
+            }],
+            ['戒指', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][2]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['对戒', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][19]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['裸钻', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][15]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['戒托', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][12]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['项链', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][4]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['吊坠', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][5]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['手链', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][8]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['手镯', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][9]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['耳钉', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][6]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['耳环', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][7]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['挂件', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][17]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['摆件', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][16]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+            ['其他', 'id', 'function', function($row) use($typeMap) {
+                $typeData = $typeMap[$row->id][18]??[];
+                return sprintf("%d", $typeData['count']??0);
+            }],
+        ];
+
+        return ExcelHelper::exportData($list, $header, '订单统计导出_' . date('YmdHis',time()));
     }
 }
