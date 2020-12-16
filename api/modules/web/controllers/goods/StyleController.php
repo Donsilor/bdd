bbcbc7d54c7dde68c3ae4a89c0b938c94c39669b
祝.福 <fuzhuzheng@163.com>
2020-12-16 17:39:02 +0800

    dev_494

--- api/modules/web/controllers/goods/StyleController.php
+++ api/modules/web/controllers/goods/StyleController.php
@@ -413,5 +413,50 @@ class StyleController extends OnAuthController
         $redis->set($key, \GuzzleHttp\json_encode($data));
 
 
+
+        //排序
+        $order = 'm.virtual_volume desc ,m.id desc';
+
+        $area_id = $this->getAreaId();
+        $fields = ['m.id','m.type_id','lang.style_name','m.goods_images','IFNULL(markup.sale_price,m.sale_price) as sale_price'];
+        $query = Style::find()->alias('m')->select($fields)
+            ->leftJoin(StyleLang::tableName().' lang',"m.id=lang.master_id and lang.language='".$this->language."'")
+            ->leftJoin(StyleMarkup::tableName().' markup', 'm.id=markup.style_id and markup.area_id='.$area_id)
+            ->where(['m.status'=>StatusEnum::ENABLED])
+            ->andWhere(['or',['=','markup.status',1],['IS','markup.status',new \yii\db\Expression('NULL')]])
+            ->orderby($order);
+
+        foreach ($data as $datum) {
+
+            $query ->orWhere(['and', ['=', 'm.type_id', $datum['c']], ['=', 'm.id', $datum['s']]]);
+        }
+
+        $result = $this->pagination($query, $this->page, $this->pageSize);
+
+        foreach($result['data'] as & $val) {
+            $arr = array();
+            $arr['id'] = $val['id'];
+            $arr['categoryId'] = $val['type_id'];
+            $arr['coinType'] = $this->getCurrencySign();
+            $arr['goodsImages'] = ImageHelper::goodsThumbs($val['goods_images'],'mid');
+            $arr['salePrice'] = $this->exchangeAmount($val['sale_price'],0);
+            $arr['goodsName'] = $val['style_name'];
+            $arr['isJoin'] = null;
+            $arr['showType'] = 2;
+            $arr['specsModels'] = null;
+
+            $arr['coupon'] = [
+                'type_id' => $val['type_id'],//产品线ID
+                'style_id' => $val['id'],//款式ID
+                'price' => $arr['salePrice'],//价格
+                'num' =>1,//数量
+            ];
+
+            $val = $arr;
+        }
+
+        CouponService::getCouponByList($this->getAreaId(), $result['data']);
+
+        return $result['data'];
     }
 }
