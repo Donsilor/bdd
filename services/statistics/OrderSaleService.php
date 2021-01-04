3db8652783876ce3e3e389798f166b7dda6c4490
祝.福 <fuzhuzheng@163.com>
2021-01-04 09:24:21 +0800

    dev_503

--- services/statistics/OrderSaleService.php
+++ services/statistics/OrderSaleService.php
@@ -5,12 +5,41 @@ namespace services\statistics;
 
 
 use common\components\Service;
+use common\models\statistics\OrderSale;
+use yii\web\UnprocessableEntityHttpException;
 
 class OrderSaleService extends Service
 {
     //生成统计数据(检查更新数据,当天，数天，当月，数月)
+    public function gen()
+    {
+
+    }
 
     //获取数据（开始时间，结束时间）
+    public function getData($start_time, $end_time)
+    {
+
+    }
+
     //删除缓存数据
-    //写入数据
+    public function delCache()
+    {
+        OrderSale::deleteAll(['is_cache'=>1]);
+    }
+
+    /**
+     * 写入数据
+     * @param $data
+     * @throws UnprocessableEntityHttpException
+     */
+    public function insertData($data)
+    {
+        $orderSale = new OrderSale($data);
+        $result = $orderSale->save();
+
+        if(!$result) {
+            throw new UnprocessableEntityHttpException($this->getError($orderSale));
+        }
+    }
 }
