4c533f2b12953da72bedb0e2a6b02c5bbcde7586
祝.福 <fuzhuzheng@163.com>
2021-01-26 15:00:22 +0800

    dev_511

--- common/models/goods/Diamond.php
+++ common/models/goods/Diamond.php
@@ -60,7 +60,11 @@ class Diamond extends \yii\db\ActiveRecord
     {
         return [
             [['id','goods_num', 'shape', 'source_id', 'is_stock', 'status', 'created_at', 'updated_at','onsale_time','type_id','goods_id','sale_volume','virtual_volume','virtual_clicks','goods_clicks', 'hk_status', 'tw_status', 'cn_status', 'us_status'], 'integer'],
-            [['sale_price','source_id','shape','goods_num','goods_sn', 'carat', 'clarity', 'cut', 'color', 'symmetry', 'polish', 'fluorescence'], 'required'],
+            [['sale_price','source_id',
+//                'shape',
+                'goods_num','goods_sn', 'carat', 'clarity',
+//                'cut',
+                'color', 'symmetry', 'polish', 'fluorescence'], 'required'],
             [['goods_num','market_price', 'sale_price', 'cost_price', 'carat', 'source_discount','length','width','aspect_ratio'], 'number'],
             ['sale_price','compare','compareValue' => 0, 'operator' => '>'],
             ['market_price','compare','compareValue' => 0, 'operator' => '>'],
